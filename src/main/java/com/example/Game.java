a63d332447d13da193a91b2eb96810eeadfe6bb6
Win-ther <emil.winther@hotmail.com>
2024-01-18 17:19:39 +0100

    Add Game class to start TDD for uppgift2

---
+++ src/main/java/com/example/Game.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Game {
+    public int score(){
+        return 0;
+    }
+    public void roll(int pinsKnockedDown){
+
+    }
+}
