93005be94e752acdad18662ee3757f3bc885bb7d
Win-ther <emil.winther@hotmail.com>
2024-01-18 12:14:16 +0100

    Add EmployeeDatabase
    EmployeeDatabase implements EmployeeRepository and its methods.

---
+++ src/main/java/com/example/EmployeeDatabase.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDatabase implements EmployeeRepository{
+    private List<Employee> employees;
+    public EmployeeDatabase(){
+        employees = new ArrayList<>();
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employees;
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        employees.stream()
+                .filter(employee -> employee.getId().equals(e.getId()))
+                .findFirst()
+                .ifPresentOrElse(employee -> employees.add(employees.indexOf(employee),e), () -> employees.add(e));
+        return e;
+    }
+}
